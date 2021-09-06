ffdcf5bc3b837a34a1a6680b000bb797dac0eaf7
diegoadal <diegoadal_13@hotmail.com>
2021-09-06 09:25:38 -0500

    rev 1.1
    punto 1. color blink listo
    punto 7. funcion de inicio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 //punto 1. cambio de color del titulo
+function colorBlink(){
+  $(".main-titulo").animate({
+    opacity: "1",
+  }, {
+    step: function(){
+      $(this).css("color", "yellow");
+    },
+    queue: true
+  }, 1000)
+  .delay(1000)
+  .animate({
+    opacity: "1",
+  }, {
+    step: function(){
+      $(this).css("color", "white");
+      //.delay(1000)
+      colorBlink();
+    },
+    queue: true
+  }, 1000)
+}
 
 //punto 2. generar numeros aleatorios y rellenar tablero
 
@@ -8,6 +29,13 @@
 
 //punto 5. contador de movimientos
 
-//punto 6. boton de Reiniciar
-
+//punto 6. boton de Iniciar
+//preparar juego
+$(function(){
+  startGame();
+});
+//iniciar juego
+function startGame(){
+  colorBlink();
+}
 //punto 7. interaccion del usuario drag & drop
