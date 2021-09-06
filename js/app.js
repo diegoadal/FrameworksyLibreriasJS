f42a0805bdd0da38002d571414c255d1c19bbf55
diegoadal <diegoadal_13@hotmail.com>
2021-09-06 09:16:41 -0500

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//punto 1. cambio de color del titulo
+
+//punto 2. generar numeros aleatorios y rellenar tablero
+
+//punto 3. verificar dulces >= 3 unidades, eliminar y sumar puntaje
+
+//punto 4. temporizador
+
+//punto 5. contador de movimientos
+
+//punto 6. boton de Reiniciar
+
+//punto 7. interaccion del usuario drag & drop
